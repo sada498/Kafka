2107ffbdaff1be872d1c7cc3a664602067cd09fc
sada498 <sada.y@outlook.com>
2020-12-31 11:08:46 +1100

    Initial commit

---
+++ kafka-simple-producer/src/main/java/com/sada/kafkajava/kafkasimpleproducer/KafkaSimpleProducerApplication.java
@@ -0,0 +1,14 @@
+package com.sada.kafkajava.kafkasimpleproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSimpleProducerApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(KafkaSimpleProducerApplication.class, args);
+		System.out.println("hello word!");
+	}
+
+}
