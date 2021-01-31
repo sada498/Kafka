5fce1175562c79730f20a28a89f9feb68813d388
sada498 <sada.y@outlook.com>
2021-01-31 13:44:46 +1100

    kafka streaming

---
+++ kafka-streaming/src/test/java/com/sada/kafkastreaming/KafkaStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sada.kafkastreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
